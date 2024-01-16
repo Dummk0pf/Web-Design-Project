36fb639bfd323818706fabf89c0f7fe245033cf3
Dummk0pf <myothermailid1@gmail.com>
2024-01-16 19:15:47 +0530

    signin

---
+++ user_Account.js
@@ -0,0 +1,15 @@
+"use-strict";
+
+// Elements
+
+const userEmail = document.getElementById("user-email");
+const userPassword = document.getElementById("user-password");
+const signInButton = document.getElementById("signin-button");
+
+window.addEventListener("keydown", function(event){
+    if(event.key === "Enter"){
+        let email = userEmail.value;
+        let password = userPassword.value;
+        
+    }
+})
